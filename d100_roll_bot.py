6d2f03f96094656a01d77b471efd99db6274308f
KulikovPetr <84563921+KulikovPetr@users.noreply.github.com>
2024-03-09 10:51:16 +0300

    Добавил диалоговые кнопки, чтобы не писать команды, а чтобы можно было нажать на кнопку и выбрать режим.

--- d100_roll_bot.py
+++ d100_roll_bot.py
@@ -1,6 +1,6 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
 
 # загрузка токена бота из файла
 def bot_token_download():
@@ -29,11 +29,18 @@ d100_failure_list = d100_failure_download()
 bot = Bot(token=bot_token_download())
 dp = Dispatcher()
 
+# Создаем объекты кнопок
+button_success = KeyboardButton(text='Успех')
+button_failure = KeyboardButton(text='Провал')
+
+# Создаем объект клавиатуры, добавляя в него кнопки
+keyboard = ReplyKeyboardMarkup(keyboard=[[button_success, button_failure]],resize_keyboard=True)
 
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут d100-криты-Бот!\nНапиши мне число, и я отвечу на него значением крита!')
+    await message.answer('Привет!\nМеня зовут d100-криты-Бот!\nНапиши мне число, и я отвечу на него значением крита! Выбери режим:', reply_markup=keyboard)
+
     # регистрация пользователя в словаре по id
     if message.from_user.id not in users:
         users[message.from_user.id] = {'d_100_mode': None}
@@ -44,19 +51,19 @@ async def process_start_command(message: Message):
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
-        'Напиши мне число от 1 до 100, и я отвечу на него значением крита!'
+        'Выбери режим, напиши мне число от 1 до 100, и я отвечу на него значением крита!', reply_markup=keyboard
     )
 
-# Этот хэндлер будет срабатывать на команду "/успех"
-@dp.message(Command(commands=['успех']))
+# Этот хэндлер будет срабатывать на сообщение с клавиатуры "Успех"
+@dp.message(F.text == 'Успех')
 async def process_roll_success_command(message: Message):
     users[message.from_user.id]['d_100_mode'] = 'success'
     await message.answer(
         'О, так ты кританул? Ну-ка, и что у тебя на д100?'
     )
 
-# Этот хэндлер будет срабатывать на команду "/провал"
-@dp.message(Command(commands=['провал']))
+# Этот хэндлер будет срабатывать на сообщение с клавиатуры "Провал"
+@dp.message(F.text == 'Провал')
 async def process_roll_failure_command(message: Message):
     users[message.from_user.id]['d_100_mode'] = 'failure'
     await message.answer(
@@ -71,11 +78,11 @@ async def send_d100_crit_success(message: Message):
         await message.answer('Сначала зарегестрируйся, нажав /start!')
     else:
         if users[message.from_user.id]['d_100_mode'] == 'success':
-            await message.answer(d100_success_list[int(message.text)-1])
+            await message.answer(d100_success_list[int(message.text)-1], reply_markup=keyboard)
         elif users[message.from_user.id]['d_100_mode'] == 'failure':
-            await message.answer(d100_failure_list[int(message.text)-1])
+            await message.answer(d100_failure_list[int(message.text)-1], reply_markup=keyboard)
         else:
-            await message.answer('Сначала выбери /успех или /провал, а потом уже пиши число')
+            await message.answer('Сначала выбери "Успех" или "Провал", а потом уже пиши число', reply_markup=keyboard)
 
 # Этот хэндлер будет срабатывать на остальные любые сообщения
 @dp.message()
